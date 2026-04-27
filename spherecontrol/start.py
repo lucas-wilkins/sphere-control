adf229188d95e410139ca796279efccc7f201f30
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2026-04-27 07:06:46 +0100

    Set logging level

--- spherecontrol/start.py
+++ spherecontrol/start.py
@@ -1,3 +1,9 @@
+import logging
 from nexus import Nexus
+
+logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s] %(asctime)s, %(name)s: %(message)s"
+    )
 
 Nexus(True)
